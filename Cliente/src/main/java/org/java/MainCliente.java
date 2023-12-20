3eca328fc4989be1ff2fab94bae3ed4024555f2e
Dolmayan <alejandrodolmayan@gmail.com>
2023-12-20 12:26:01 -0600

    Sockets Examples

---
+++ Cliente/src/main/java/org/java/MainCliente.java
@@ -0,0 +1,15 @@
+package org.java;
+
+import org.java.cliente.Cliente;
+
+import java.io.IOException;
+
+public class MainCliente {
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+}
